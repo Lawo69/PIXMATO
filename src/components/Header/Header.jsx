e9274f075a4359aa78cddde134a3b818c72e78c8
Supun Gunawardana <supungunawardana.me@gmail.com>
2023-11-12 13:37:12 +0530

    create cart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,6 +14,7 @@ import "./Header.scss";
 const Header = () => {
 
     const [scrolled, setScrolled] = useState(false);
+    const [showCart, setShowCart] = useState(false);
     const handleScroll = () => {
         const offset = window.scrollY;
         if (offset > 200) {
@@ -27,24 +28,29 @@ const Header = () => {
         window.addEventListener("scroll", handleScroll)
     }, []);
 
-    return <header className={`main-header ${scrolled ? 'sticky-header' : ""}`}>
-        <div className="header-content">
-            <ul className="left">
-                <li>Home</li>
-                <li>About</li>
-                <li>Categories</li>
-            </ul>
-            <div className="center">PIXMAT<span>O</span></div>
-            <div className="right">
-                <TbSearch />
-                <AiOutlineHeart />
-                <span className="cart-icon">
-                    <CgShoppingCart />
-                    <span>5</span>
-                </span>
-            </div>
-        </div>
-    </header>;
+    return (
+        <>
+            <header className={`main-header ${scrolled ? 'sticky-header' : ""}`}>
+                <div className="header-content">
+                    <ul className="left">
+                        <li>Home</li>
+                        <li>About</li>
+                        <li>Categories</li>
+                    </ul>
+                    <div className="center">PIXMAT<span>O</span></div>
+                    <div className="right">
+                        <TbSearch />
+                        <AiOutlineHeart />
+                        <span className="cart-icon" onClick={() => setShowCart(true)}>
+                            <CgShoppingCart />
+                            <span>5</span>
+                        </span>
+                    </div>
+                </div>
+            </header>
+            {showCart && <Cart setShowCart={setShowCart} />}
+        </>
+    );
 };
 
 export default Header;
