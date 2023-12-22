f760eb8fbfd28a78d811d375dcc62290df1433d9
Supun Gunawardana <supungunawardana.me@gmail.com>
2023-12-23 00:00:52 +0530

    shopping cart add

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -16,6 +16,9 @@ const Header = () => {
     const [scrolled, setScrolled] = useState(false);
     const [showCart, setShowCart] = useState(false);
     const [showSearch, setShowSearch] = useState(false);
+    const { cartCount } = useContext(Context);
+    const navigate = useNavigate();
+
     const handleScroll = () => {
         const offset = window.scrollY;
         if (offset > 200) {
@@ -34,17 +37,17 @@ const Header = () => {
             <header className={`main-header ${scrolled ? 'sticky-header' : ""}`}>
                 <div className="header-content">
                     <ul className="left">
-                        <li>Home</li>
-                        <li>About</li>
-                        <li>Categories</li>
+                        <li onClick={() => navigate("/")}>Home</li>
+                        <li onClick={() => navigate("/")}>About</li>
+                        <li onClick={() => navigate("/")}>Categories</li>
                     </ul>
-                    <div className="center">PIXMAT<span>O</span></div>
+                    <div className="center"  onClick={() => navigate("/")}>PIXMAT<span>O</span></div>
                     <div className="right">
                         <TbSearch onClick={() => setShowSearch(true)}/>
                         <AiOutlineHeart />
                         <span className="cart-icon" onClick={() => setShowCart(true)}>
                             <CgShoppingCart />
-                            <span>5</span>
+                            {!!cartCount && <span>{cartCount}</span>}
                         </span>
                     </div>
                 </div>
